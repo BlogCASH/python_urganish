9abd98e3633f977dd37d381ef25f2b80a74b3c80
BlogCASH <playzone937@gmail.com>
2025-10-25 16:33:23 +0500

    lambda

--- rekursiv.py
+++ rekursiv.py
@@ -1,7 +1,12 @@
-def sanash(son):
-    if son == 0:
-        return 
-    print(son)
-    sanash(son-1)
+# def sanash(son):
+#     if son == 0:
+#         return 
+#     print(son)
+#     sanash(son-1)
+# sanash(20)
+# 17-misol
+x=lambda a,b: a**b
+print(x(10,2))
 
-sanash(20)
+
+        
