6a2b97a58c704a20215f8720ac52d787cea493a6
BlogCASH <playzone937@gmail.com>
2025-10-28 16:35:15 +0500

    rekuriya masalalar

--- rekursiv.py
+++ rekursiv.py
@@ -5,8 +5,59 @@
 #     sanash(son-1)
 # sanash(20)
 # 17-misol
-x=lambda a,b: a**b
-print(x(10,2))
+# x=lambda a,b: a**b
+# print(x(10,2))
 
 
-        
+        # lambda bilan ishlash tugatildi
+
+# x=lambda a,b: a+b
+# print(x(25,25))
+# 1-misol
+# def factarialni_top(n):
+#     if n ==1:
+#         return 1
+#     else :
+#         return factarialni_top(n-1)*n 
+# print(factarialni_top(12))   
+# 2-misol
+# def fibananchi_ketmaketligini_top(son):
+#     if son ==1 or son ==2 :
+#         return 1 
+#     else:
+#         return fibananchi_ketmaketligini_top(son-1) + fibananchi_ketmaketligini_top(son-2)
+# print(fibananchi_ketmaketligini_top(20))
+# 3-misol
+# def raqam_yigindisini_top(n):
+#     if n<10 :
+#         return n
+#     else :
+#         return n%10 + raqam_yigindisini_top(n//10)
+# print(raqam_yigindisini_top(45))
+# 4-misol
+# def oxirgi_raqamni_top(n):
+#     if n<10 :
+#         return n
+#     else :
+#         return n%10 
+# print(oxirgi_raqamni_top(22))
+# 5-misol
+# def son_nechtadan_iboratligini_top(n):
+#     if n ==0:
+#         return 0
+#     else :
+#         return 1 + son_nechtadan_iboratligini_top(n//10)
+# print(son_nechtadan_iboratligini_top(1234))
+# 6-misol
+# def teskari_tartibla(matn):        
+    # if matn == "":
+    #     return ""
+    # else :
+        # return matn [-1] + teskari_tartibla(matn[:-1])
+# print(teskari_tartibla("munisbek"))
+# # 7-misol
+# def palindrom_satr(matn):
+#     if
+
+
+
